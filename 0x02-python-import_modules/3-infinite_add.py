72c7b6080fe622e41bd925b67f19eeeace9c6c95
Sinofipaza <fipazasino@gmail.com>
2023-08-11 08:47:12 -0700

    python3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from sys import argv
 if __name__ == "__main__":
-    sum = 0
-    for i in range(1, len(argv)):
-        sum += int(argv[i])
-    print(sum)
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-import hidden_4
 if __name__ == "__main__":
-    list = []
-    for i in dir(hidden_4):
-        if i[0] != '_':
-            list.append(i)
-    for i in list:
-        print(i)
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
