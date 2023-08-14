dbcaa71e83245f2eb54f5da989f2d9f2d404854b
Sinofipaza <fipazasino@gmail.com>
2023-08-14 06:23:17 -0700

    python

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list) - 1:
+        return my_list
     else:
         new_list = []
         for i in my_list:
