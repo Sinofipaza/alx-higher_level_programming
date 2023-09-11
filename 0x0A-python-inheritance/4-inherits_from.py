e8c1d6f41ca892b1bf88869c95d219453eecc72a
Sino <fipazasino@gmail.com>
2023-09-11 20:28:01 +0200

    py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 def inherits_from(obj, a_class):
-    return isinstance(obj, a_class) and type(obj) != a_class
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
