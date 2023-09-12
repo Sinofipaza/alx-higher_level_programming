5af6ada200ca4b7c3a2d0b6cb67fad56ebde2961
Sinofipaza <fipazasino@gmail.com>
2023-09-12 15:14:17 +0000

    py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_of_lines(filename=""):
+    """ count lines in file """
+    count = 0
+    if filename == "":
+        return count
+    with open(filename, "r") as f:
+        for l in f:
+            count += 1
+    return count
