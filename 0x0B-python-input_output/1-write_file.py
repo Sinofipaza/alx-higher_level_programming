b01575787ce276bd331df8da92bbe37e7986250f
Sinofipaza <fipazasino@gmail.com>
2023-09-12 15:16:24 +0000

    py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,3 +9,8 @@ def number_of_lines(filename=""):
         for l in f:
             count += 1
     return count
+
+write_file = __import__('1-write_file').write_file
+
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
