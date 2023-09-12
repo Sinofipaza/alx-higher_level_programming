685a3dc0ee5ca2ff035babd0a88dde0912fc2b2d
Sinofipaza <fipazasino@gmail.com>
2023-09-12 15:11:10 +0000

    py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """ prints file contents """
+    if filename == "":
+        return
+    with open(filename, "r") as f:
+        print(f.read(), end="")
