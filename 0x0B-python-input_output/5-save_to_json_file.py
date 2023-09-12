62fbf3b11f4ee0d862c75ff2529441d164c24b7b
Sinofipaza <fipazasino@gmail.com>
2023-09-12 15:52:07 +0000

    py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+JSON representation of string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ return JSON representation of string """
+    return json.dumps(my_obj)
